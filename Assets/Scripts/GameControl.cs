2d9519bd19d3ad6f97e3f1ae1ac09b951f420991
Fredrik Johansson <f95johansson@gmail.com>
2018-11-20 16:54:18 +0100

    Added game progress save and such

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -17,6 +17,7 @@ public class GameControl : MonoBehaviour
     public Inventory Inventory = new Inventory();
     public PlayerData PlayerData = new PlayerData();
     public ShopData ShopData = new ShopData();
+    public GameProgress Progress = new GameProgress();
 
 
     private void Awake()
@@ -26,11 +27,18 @@ public class GameControl : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
             Control = this;
+            LoadProgress();
+            print(Progress.ToString());
         }
         else if (Control != this)
         {
             Destroy(gameObject);
         }
+    }
+
+    void OnApplicationPause() // pause because iOS does not call quit
+    {
+        SaveProgress();
     }
 
     public void LoadInventory()
@@ -141,7 +149,25 @@ public class GameControl : MonoBehaviour
         };
             
 
-    bf.Serialize(file, data);
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void LoadProgress() {
+        var bf = new BinaryFormatter();
+        if (File.Exists(userDataPath + "/Progress.dat"))
+        {
+            var file = File.Open(userDataPath + "/Progress.dat", FileMode.Open);
+            var data = (ProgressData) bf.Deserialize(file);
+            file.Close();
+            Progress = GameProgress.FromSerialized(data);
+        }  
+    }
+
+    public void SaveProgress() {
+        var bf = new BinaryFormatter();
+        var file = File.Create(userDataPath + "/Progress.dat");
+        bf.Serialize(file, Progress.Serialize());
         file.Close();
     }
 
