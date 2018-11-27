008525177f3dec3bba9375518bdcb7159b6b829b
Fredrik Johansson <f95johansson@gmail.com>
2018-11-27 14:37:05 +0100

    Level select should now dissplay track progress

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public string Axis = "x";
+    public float RotateSpeed = 20f;
+
+    private void Update ()
+    {
+        if (Axis == "x") {
+            transform.Rotate(new Vector3(RotateSpeed * Time.deltaTime, 0, 0));
+        } else if (Axis == "y") {
+            transform.Rotate(new Vector3(0, RotateSpeed * Time.deltaTime, 0));
+        } else if (Axis == "z") {
+            transform.Rotate(new Vector3(0, 0, RotateSpeed * Time.deltaTime));  
+        }
+    }
+}
