d8fdf6f1bdc076cf43a6393873ed98d22636e182
Fredrik Johansson <f95johansson@gmail.com>
2018-12-01 20:23:15 +0100

    Tried to fix some prefabs that was accedently overwritten

--- Assets/Scripts/TrackWrapper/StarSystem.cs
+++ Assets/Scripts/TrackWrapper/StarSystem.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StarSystem : MonoBehaviour
 {
     public WinCondition WinCondition;
+    public Image GoButton;
 
     private Text _goal;
     private Text _status;
@@ -26,5 +29,46 @@ public class StarSystem : MonoBehaviour
         });
         
         WinCondition.OnStateChange().AddListener(() => { _status.text = WinCondition.ChangedState(); });
+
+        WinCondition.OnTestWin().AddListener(() => { 
+            Color buttonColor = GoButton.color;
+            Color blinkColor = new Color(99/255f, 224/255f, 65/255f);
+            StartCoroutine(blink(GoButton, .3f, buttonColor, blinkColor, () => {
+                StartCoroutine(blink(GoButton, .3f, buttonColor, blinkColor, () => {
+                    StartCoroutine(delayedBlink(1, GoButton, .3f, buttonColor, blinkColor, () => {
+                        StartCoroutine(blink(GoButton, .3f, buttonColor, blinkColor, () => {}));    
+                    }));
+                }));
+            }));
+        });
+
+        
+    }
+
+    private IEnumerator delayedBlink(float waitTime, Image image, float time, Color color1, Color color2, Action after) {
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(blink(image, time, color1, color2, after));
+    }
+
+    private IEnumerator blink(Image image, float time, Color color1, Color color2, Action after)
+    {
+        var timeLeft = time/2;
+        image.color = color1;
+        while (timeLeft > 0) {
+            timeLeft -= Time.fixedDeltaTime;
+            image.color = Color.Lerp(color1, color2, 1-timeLeft/(time/2));
+            yield return new WaitForFixedUpdate();
+        }
+        image.color = color2;
+        timeLeft = time/2;
+        while (timeLeft > 0) {
+            timeLeft -= Time.fixedDeltaTime;
+            image.color = Color.Lerp(color2, color1, 1-timeLeft/time);
+            yield return new WaitForFixedUpdate();
+        }
+        image.color = color1;
+        
+        after();
+        yield return null;
     }
 }
