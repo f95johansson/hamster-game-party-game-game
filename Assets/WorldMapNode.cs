9f9fee8cc3041366c875d4928f3755df2639afe9
Oskar Olausson <oskar.erik.olausson@gmail.com>
2018-12-05 16:28:31 +0100

    cool tracks and new level select

---
+++ Assets/WorldMapNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMapNode : MonoBehaviour {
+	private void OnMouseUpAsButton()
+	{
+		Navigation.StartTrack(gameObject.name);
+	}
+
+	private const float BIG = 1.4f;
+	private float _normal = 1.4f;
+	private float _goal;
+	private float _current;
+
+	private void Start()
+	{
+		_normal = transform.localScale.x;
+		_goal = _normal;
+		_current = 0;
+
+		if (GameControl.Control.Progress.HasCleared(gameObject.name))
+		{
+			var star = GetComponentsInChildren<SpriteRenderer>()[1];
+			star.enabled = true;
+		}
+	}
+	
+	private void OnMouseEnter()
+	{
+		_goal = BIG;
+	}
+
+	private void OnMouseExit()
+	{
+		_goal = _normal;
+	}
+
+	private void Update()
+	{
+		_current = Mathf.Lerp(_current, _goal, .1f);
+		transform.localScale = new Vector3(_current, _current, transform.localScale.z);
+	}
+}
